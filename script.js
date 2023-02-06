b8360829dee6638afa871264149e8f99a113491b
Abayomi Balogun <109353177+abayomi2010@users.noreply.github.com>
2023-02-06 00:19:51 +0000

    extracted the temp, humidity and city name for the five-day forecast from the endpoint, and appended it to the dashboard

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let searchInput = $("#search-input");
 let searchForm = $("#search-form");
 let searchHistory = [];
 let searchHistoryCont = $("#history");
+let forecastContainer = $("#forecast");
 let todayContainer = $("#today");
 
 
@@ -26,7 +27,7 @@ function renderSearchHistory () {
                let btn = $("<button>");
 
             // give each button a type and data search attributes with values as button and searchHistory respectively
-               btn.attr({type: "button", dataSearch :"searchHistory"});
+               btn.attr({type: "button", dataSearch :searchHistory[i]});
 
             //  give each button class = btn-history
                btn.addClass("btn-info");
@@ -90,6 +91,83 @@ function renderCurrentWeather(city, weatherData) {
          }
 
 
+
+
+function renderForecast (weatherData) {
+    let headingCol = $("<div>");
+    let heading = $("<h4>");
+
+    headingCol.attr("class", "col-12");
+    heading.text("5-Day Forecast");
+    headingCol.append(heading);
+    
+    forecastContainer.html("")
+    
+    forecastContainer.append(headingCol);
+    
+    
+    let futureForecast = weatherData.filter(function (forecast) {
+        return forecast.dt_txt.includes("12");
+    })
+
+
+    for (let i = 0; i < futureForecast.length; i++) {
+        let iconUrl = `https://openweathermap.org/img/wn/${futureForecast[i].weather[0]["icon"]}.png`;
+        
+        let iconDescription = futureForecast[i].weather[0]["description"];
+
+        let tempC = futureForecast[i].main.temp;
+        let humidity = futureForecast[i].main.humidity;
+        let windKph = futureForecast[i].wind.speed;
+
+        let col = $("<div>");
+        let card =$("<div>");
+        let cardBody = $("<div>");
+        let cardTitle = $("<h5>");
+        let weatherIcon = $("<img>");
+        let tempEl = $("<p>");
+        let windEl = $("<p>");
+        let humidityEl = $("<p>");
+
+        col.append(card);
+        card.append(cardBody);
+        cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+
+        col.attr("class", "col-md");
+        card.attr("class", "card bg-primary h-100 text-white");
+        cardTitle.attr("class", "card-title");
+        tempEl.attr("class", "card-text");
+        windEl.attr("class", "card-text");
+        humidityEl.attr("class", "card-text");
+        
+
+        cardTitle.text(moment(futureForecast[i].dt_text).format("D/M/YYYY"));  
+        weatherIcon.attr("src", iconUrl);
+        weatherIcon.attr("alt", iconDescription);
+        tempEl.text(`Temperature:  ${tempC} °C`);
+        windEl.text(`Wind Speed: ${windKph} KPH`);
+        humidityEl.text(`Humidity: ${humidity}%`);
+
+
+        forecastContainer.append(col);
+        
+        
+        
+        
+        
+        
+
+        
+        
+        
+
+
+    }
+
+}
+
+
+
 function fetchWeather (location) {
     let latitude = location.lat;
     let longitude = location.lon;
@@ -101,10 +179,12 @@ function fetchWeather (location) {
         url : queryWeatherURL,
         method : "GET"
     }).then(function (response){
-
+        
         
         renderCurrentWeather(city, response.list[0]);
-        // renderForecast(data.list)
+        renderForecast(response.list)
+        // console.log(response.list);
+        
         
         
     })
@@ -138,15 +218,20 @@ function initialiseSearchHistory () {
 }
 
 
-function handleSearchFormSubmitHistory(event) {
-    if (!$(event.target).hasClass("btn-history")) {
+function handleSearchHistory(event) {
+    if (!$(event.target).hasClass("btn-info")) {
         return;
     }
     let search = $(event.target).attr("dataSearch");
-    console.log(search);
+    
+
+    fetchCoordinates(search);
+    searchInput.val("");
+    
     
     
 }
+
 
 initialiseSearchHistory();
 
@@ -158,3 +243,5 @@ searchForm.on("submit", function(event) {
     searchInput.val("");
 
 })
+
+searchHistoryCont.on("click",  handleSearchHistory);
